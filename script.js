f33198084a85fd2d79f67aeb2a1135a6e549189f
CrazyyKat <katchengfamily@gmail.com>
2024-07-28 00:49:08 -0700

    trying to get the animation to work and failing

---
+++ script.js
@@ -0,0 +1,14 @@
+// (function () {
+//     var words = [
+//         "The Mini",
+//         "Wordle",
+//         "Connections",
+//         "The Mini"
+//     ],
+//     i = 0;
+//     setInterval(function(){
+//         $('#words').fadeOut(function(){
+//             $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
+//         });
+//     }, 1000)
+// })();
